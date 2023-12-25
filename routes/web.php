fe636394ba5fd944bcbe8ae230f433036ac2b5a2
YungBenn <adisuryo22@gmail.com>
2023-12-25 14:13:56 +0700

    tutorial commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-
-Route::get('/', function () {
-    return redirect('/posts');
-});
-
-Route::resource('/posts', PostController::class);
